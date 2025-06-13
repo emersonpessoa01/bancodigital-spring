32587cfe0c920a41f8375dcc2f8cc612737ec63e
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-13 11:53:55 -0300

    fix: Added path in AddCliente :sparkles:

--- src/main/java/br/com/cdb/bancodigital_spring/controller/ClienteController.java
+++ src/main/java/br/com/cdb/bancodigital_spring/controller/ClienteController.java
@@ -8,16 +8,23 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/cliente")
-public class ClienteController
-{
+public class ClienteController {
     private ClienteService clienteService = new ClienteService();
+
     @PostMapping("/add")
     public void addCliente(@RequestBody Cliente cliente) {
         // Chama o serviço para adicionar o cliente
         clienteService.addCliente(cliente.getNome(), cliente.getCpf());
     }
+
+    @PostMapping("/add/{nome}/{cpf}")
+    public void addCliente(@PathVariable String nome, @PathVariable long cpf) {
+        // Chama o serviço para adicionar o cliente
+        clienteService.addCliente(nome, cpf);
+    }
+
     @GetMapping("/all")
-    public ArrayList<Cliente> getAllClientes(){
+    public ArrayList<Cliente> getAllClientes() {
         // Chama o serviço para obter todos os clientes
         return clienteService.getClientes();
     }
